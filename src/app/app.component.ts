b4c5042ec301094b5e3decd81cdb95534dae0045
Jacob Ernst <jernst4601@gmail.com>
2022-11-13 19:59:47 -0500

    testing the REST API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  message: string = 'No message found :(';
+
+  constructor(private http: HttpClient) {
+  }
+
   title = 'remindinator-client';
+
+  getMessage() {
+    this.http.post("http://localhost:8080/persons/hello", {
+      "name": "Jacob",
+      "birth": 920679310
+    }).subscribe(res => {
+      console.log(res)
+    })
+    // this.http.get<string>("http://localhost:8080/persons/hello").pipe().subscribe(res => {
+    //   console.log(res)
+    // })
+  }
 }
