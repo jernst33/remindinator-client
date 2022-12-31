ce14d5c08e077e7a7204442b634aae50c8bbda90
Jacob Ernst <jernst4601@gmail.com>
2022-12-30 20:14:58 -0500

    more work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Form, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   message: string = 'No message found :(';
+  userForm: FormGroup = new FormGroup<any>({});
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.userForm = this.fb.group({
+      name: [null, Validators.required],
+      birth: [null, Validators.required],
+      weight: [null, Validators.required],
+      human: [null, Validators.required],
+      numberOfTeeth: [null, Validators.required],
+    });
   }
 
   title = 'remindinator-client';
 
   getMessage() {
-    this.http.post("http://localhost:8080/persons/hello", {
+    this.http.post("http://localhost:8080/persons", {
       "name": "Jacob",
       "birth": 920679310
     }).subscribe(res => {
@@ -25,5 +39,18 @@ export class AppComponent {
     // this.http.get<string>("http://localhost:8080/persons/hello").pipe().subscribe(res => {
     //   console.log(res)
     // })
+  }
+
+  onSubmit() {
+    console.log(this.userForm);
+    this.http.post("http://localhost:8080/persons", {
+      name: this.userForm.get('name')?.value,
+      birth: new Date(""),
+      weight: this.userForm.get('weight')?.value,
+      human: this.userForm.get('human')?.value,
+      numberOfTeeth: this.userForm.get('numberOfTeeth')?.value,
+    }).subscribe(res => {
+      console.log(res)
+    })
   }
 }
---
+++ src/app/models/person.ts
@@ -0,0 +1,7 @@
+export interface Person {
+  name: string;
+  birth: Date;
+  weight: number;
+  human: boolean;
+  numberOfTeeth: number;
+}
